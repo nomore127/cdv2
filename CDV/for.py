4eac56b70045030fff85e9b84096e2fa043a0aa5
NOmore <mczyz@edu.cdv.pl>
2019-04-29 13:44:02 +0200

    Dodajemy

---
+++ CDV/for.py
@@ -0,0 +1,26 @@
+#pętla for
+
+colors = ['red', 'green', 'blue', 'magenta']
+print(colors)
+print(type(colors))
+
+for color in colors:
+    print (color)
+
+print('\n')
+
+#wypisz tekst
+string = 'CDV - uczelnia ludzi ciekawych'
+
+for cdv in string:
+    print (cdv,end=" ")
+print()
+#Wypisz liczby od 1 do 10
+for numer in range(1, 11):
+    print(numer,end=" ")
+print()
+
+#wypisz elementy z listy do momentu wartości 'end','q','quit'
+
+lista = ['Audi','BMW','Porshe','Mercedes','Lamborghini','quit']
+
