58d1a1090e72db1ac9a98fe9da4bc1156f217a68
NOmore <mczyz@edu.cdv.pl>
2019-04-29 12:19:10 +0200

    Dodajemy

--- CDV/71funkcje.py
+++ CDV/71funkcje.py
@@ -27,3 +27,40 @@ data1 = {
 
 print(f'Przed wywyołaniem funkcji show: {data1}')
 show(data1)
+
+####################### Przekazywanie armugentów przez wartość #######################
+print('\n########## Przekazanie przez wartość')
+
+def show1(city):
+    print(f'Przed modycfikacją: {city}')
+    city[0] = 'Berlin'
+    city[1] = 'Bukareszt'
+    print(f'Po modycfikacją: {city}')
+    print(f'Po modycfikacją: {id(city)}')
+
+miasto = ['Poznań','Gniezno']
+
+print(f'Przed wywyołaniem funkcji show1: {miasto}')
+print(f'Id obiektu show: {id(miasto)}')
+show1(list(miasto))
+print(f'Po wywyołaniem funkcji show1: {miasto}')
+
+####################### Słownik #######################
+
+def show2(abecadlo):
+    print(f'Przed modycfikacją: {abecadlo}')
+    abecadlo[0] = 'A'
+    abecadlo[1] = 'B'
+    abecadlo[2] = 'C'
+    abecadlo[3] = 'D'
+    abecadlo[4] = 'E'
+    abecadlo[5] = 'F'
+    print(f'Po modycfikacją: {abecadlo}')
+    print(f'Po modycfikacją: {id(abecadlo)}')
+
+asd = ['F','E','D','C','B','A']
+
+print(f'Przed wywyołaniem funkcji show2: {asd}')
+print(f'Id obiektu show: {id(asd)}')
+show2(list(asd))
+print(f'Po wywyołaniem funkcji show2: {asd}')
